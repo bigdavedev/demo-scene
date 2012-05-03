fd53814643c10cfa3342a7353759276a62df8310
Dave Brown <d.brown@bigdavedev.com>
2012-05-03 20:09:27 +0200

    Adds the main.c file which opens a window but has not been optimised for exe size yet

---
+++ src/main.c
@@ -0,0 +1,53 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+    MSG msg = { 0 };
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hbrBackground = (HBRUSH) (COLOR_BACKGROUND);
+    wc.lpszClassName = "a";
+    if (FAILED(RegisterClass(&wc)))
+        return 1;
+
+    if (FAILED(CreateWindow(wc.lpszClassName, "BigDaveDev Demo-Skeleton (c)2012", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 640, 480, 0, 0, hInstance, NULL)))
+        return 2;
+
+    while (1)
+    {
+        if(PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            if(msg.message == WM_QUIT)
+                break;
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+        }
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+
+}
